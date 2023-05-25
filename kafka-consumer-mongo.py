d8632dd99f069ab170850206d28fb4eeb6338bc4
lokenmg <104791261+lokenmg@users.noreply.github.com>
2023-05-25 10:16:40 -0600

    Update kafka-consumer-mongo.py

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -49,22 +49,20 @@ for msg in consumer:
        print("Data inserted with record ids", pelicula_id)
     except:
        print("Could not insert into MongoDB")
-    
-    
-try:
-    ago_result =db.peliculas_info.agregate(
-    [{
-        "$group" :
-        {
-            "_id": "$name",
-            "n"  : {"$sum" : 1}
-        }
-    }])
-    db.peliculas_sumary.delete_many({})
-    for i in agg_result:
-        print (i)
-        sumary_id = de.peliculas_suamry.insert_one(i)
-        print("Sumary inserted with record ids", sumary_id)
-except Exception as e:
-    print(f'group by caunght {type(e)}: ')
-    print(e)
+    try:
+       ago_result =db.peliculas_info.agregate(
+        [{
+            "$group" :
+            {
+                "_id": "$name",
+                "n"  : {"$sum" : 1}
+         }
+        }])
+        db.peliculas_sumary.delete_many({})
+        for i in agg_result:
+            print (i)
+            sumary_id = de.peliculas_suamry.insert_one(i)
+            print("Sumary inserted with record ids", sumary_id)
+    except Exception as e:
+        print(f'group by caunght {type(e)}: ')
+        print(e)
